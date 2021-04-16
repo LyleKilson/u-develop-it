5fe3202c6b0c4f2a34e36e8fcb3d848ee9a7a0b9
Lyle Kilson <lylekilsongithub@gmail.com>
2021-04-16 13:24:40 -0600

    added connection to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 // Expresss middleware
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your My SQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Winnipeg2012!",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
 app.get("/", (req, res) => {
   res.json({
